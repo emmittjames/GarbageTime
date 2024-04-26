c821454f4a0f5caa9ccd458ca701c38fde1d20a4
emmittjames <emmittjames1@gmail.com>
2024-04-26 13:00:41 -0400

    clean up

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let map;
 
 async function initMap(data) {
-  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  console.log(data)
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps")
   const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker")
 
   const infoWindow = new InfoWindow();
@@ -12,11 +13,11 @@ async function initMap(data) {
     mapId: "abcde",
   });
 
-  const greenBin = document.createElement("img");
+  const greenBin = document.createElement("img")
   greenBin.src =
-    "./images/trash_can_green.png";
-  greenBin.style.width = "50px"; // Set the width to 100 pixels
-  greenBin.style.height = "60px";
+    "./images/trash_can_green.png"
+  greenBin.style.width = "50px"
+  greenBin.style.height = "60px"
 
   const greenBinPin = new PinElement({
     scale: 0.5,
@@ -30,35 +31,36 @@ async function initMap(data) {
   });
 
   marker.addListener("click", () => {
-    console.log("clicked");
+    console.log("clicked")
     infoWindow.close()
-    infoWindow.setContent(marker.title);
-    infoWindow.open(marker.map, marker);
+    infoWindow.setContent(marker.title)
+    infoWindow.open(marker.map, marker)
   });
 }
 
 async function fetchData(url) {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url)
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error("Network response was not ok")
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
+    console.error("Error fetching data:", error)
+    return null
   }
 }
 
 async function initMapWithData() {
-  apiUrl = "";
-  const data = await fetchData(apiUrl);
+  const data = await fetchData(backendUrl)
   if (data) {
-    initMap(data);
+    initMap(data)
   } else {
-    console.error('Failed to fetch data.');
+    console.error('Failed to fetch data.')
   }
 }
 
-initMapWithData("ahhhh");
+// initMapWithData();
+
+initMap("hi")
