dc116dc187b04ffa9162d725c1e6805c2809c4c0
Oscar Lauth <oscar.lauth@gmail.com>
2024-05-05 17:49:27 -0400

    Complete url

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ async function fetchData(url) {
 // initMapWithData();
 
 const getTTNData = ()=> {
-  fetch('/.netlify/functions/webhook')
+  fetch('https://garbagetime1.netlify.app/.netlify/functions/webhook')
   .then(response => response.json())
   .then(data => console.log(data));
 }
